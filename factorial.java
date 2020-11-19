6346355d9ea43b2b33ef7cebc9888fd68452b9d7
PreethiKrishnan <74711561+PreethiKrishnan2020@users.noreply.github.com>
2020-11-19 17:44:58 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=5;
+int fact=1;
+for(int i=1;i<=n;i++)
+{
+	fact=fact*i;
+//	System.out.println("Factorial of "+fact+" is "+ fact);
+}
+System.out.println("Fatorial of "+n+"is"+fact);
+	}
+
+}
